8cb8c3d0912046773a7946f51c546b9899d70df4
佳伟 <987553665@qq.com>
2023-07-18 17:48:52 +0800

    Site updated: 2023-07-18 17:48:45

--- js/cursor.js
+++ js/cursor.js
@@ -82,3 +82,13 @@ class Cursor {
     CURSOR = new Cursor();
     // 需要重新获取列表时，使用 CURSOR.refresh()
 })();
+
+
+// 防手抖
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
